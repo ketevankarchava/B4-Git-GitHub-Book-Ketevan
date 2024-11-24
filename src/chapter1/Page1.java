ac9232e47ea44076dad4548defa751e0b94e2aa9
Ketevan Karchava <ketevan0915@gmail.com>
2024-11-24 08:24:16 -0700

    added another print statement

--- src/chapter1/Page1.java
+++ src/chapter1/Page1.java
@@ -3,5 +3,6 @@ package chapter1;
 public class Page1 {
     public static void main(String[] args) {
         System.out.println("Ketevan added this line");
+        System.out.println("Ketevan added another line of code");
     }
 }
